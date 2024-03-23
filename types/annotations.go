d18582a1938ffb5881a80150ca30be9b497c960e
Brandon Mitchell <git@bmitch.net>
2024-03-23 16:11:39 -0400

    Update annotation to not use OCI values before approval
    Annotations for the referrer have not been approved by OCI, so switch to project specific values.
    
    Signed-off-by: Brandon Mitchell <git@bmitch.net>

--- types/annotations.go
+++ types/annotations.go
@@ -5,7 +5,7 @@ const (
 	AnnotRefName = "org.opencontainers.image.ref.name"
 	// AnnotReferrerSubject is used on descriptors that point to the referrer response in an OCI layout index.json.
 	// The value is the digest of the subject.
-	AnnotReferrerSubject = "org.opencontainers.image.referrer.subject"
+	AnnotReferrerSubject = "org.olareg.referrer.subject"
 	// AnnotReferrerConvert is set to true to indicate any referrers in the OCI Layout pushed with the fallback tag have been converted to the referrer.subject annotation.
-	AnnotReferrerConvert = "org.opencontainers.image.referrer.convert"
+	AnnotReferrerConvert = "org.olareg.referrer.convert"
 )
